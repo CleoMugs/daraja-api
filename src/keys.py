3d55680482466b2ea914432107e57e1a83d85ce9
CleoMugs <cleophas.mugeni@gmail.com>
2020-09-03 18:45:30 +0300

    gunicorn

---
+++ src/keys.py
@@ -0,0 +1,9 @@
+
+business_shortCode = "174379" # Lipa Na Mpesa Shortcode
+phone_number = "254708252968"
+lipa_na_mpesa_passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
+consumer_key = "u1Giuzu99b9jW0kNF2LVrLq0YST8ZruM"
+consumer_secret = "I0lMD40o7aakSaH4"
+
+shortcode = "601431"
+test_msisdn = "254708374149"
